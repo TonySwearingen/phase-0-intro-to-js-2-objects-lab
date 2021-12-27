407c27a1e6d16ca1f869d20e0e6a903dd8385d93
Tony Swearingen <unlimited1974@aol.com>
2021-12-27 15:56:18 -0600

    code pushed

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ let employee = {
     address: "address"
 };
 function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, ...{[key]: value}}
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
     
+    return employee[key];
 }
